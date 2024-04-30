04aea516e090b9a40fdba970ef246ddc13b74f29
minbbal <jashely@naver.com>
2024-04-30 18:29:06 +0900

    상세 페이지 수정중

--- lib/prayDetail.dart
+++ lib/prayDetail.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class PrayDetail extends StatefulWidget {
   final String title;
@@ -25,13 +26,44 @@ class _PrayDetailState extends State<PrayDetail> {
       ),
       body: Padding(
         padding: EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[]),
-          ],
-        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'text pray title',
+                        labelStyle: TextStyle(color: Color(0xFF14213D)),
+                        hintStyle: TextStyle(color: Color(0xFF14213D)),
+                        errorStyle: TextStyle(color: Color(0xFF14213D)),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none),
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'text pray detail',
+                          labelStyle: TextStyle(color: Color(0xFF14213D)))),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: Text('SAVE')),
+                  TextButton(onPressed: () {}, child: Text('CANCEL')),
+                ],
+              )
+            ]),
       ),
     );
   }
