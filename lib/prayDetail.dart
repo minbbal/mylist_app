467ec5406025205128ebc2087f393b456d87879e
minbbal <jashely@naver.com>
2024-08-16 18:24:34 +0900

    add detail data apply sharedPreference

--- lib/prayDetail.dart
+++ lib/prayDetail.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PrayDetail extends StatefulWidget {
   final String title;
@@ -12,6 +13,43 @@ class PrayDetail extends StatefulWidget {
 }
 
 class _PrayDetailState extends State<PrayDetail> {
+  // shared preference 인스턴스 생성
+  Future<SharedPreferences> get _prefs => SharedPreferences.getInstance();
+  late Future<List<String>> _prayTitle;
+  late Future<List<String>> _prayDetail;
+
+  // 플로팅 액션 버튼 클릭 이벤트
+  Future<void> _incrementCounter() async {
+    final SharedPreferences prefs = await _prefs;
+    // counter 값이 존재하지 않으면 0으로 저장
+    final List<String> prayTitle = (prefs.getStringList('prayTitle') ?? [""]);
+    final List<String> prayDetail =
+        (prefs.getStringList('_prayDetail') ?? [""]);
+
+    // 앱의 상태 변경, 클릭시 카운트 +1
+    setState(() {
+      _prayTitle =
+          prefs.setStringList('prayTitle', prayTitle!).then((bool success) {
+        return _prayTitle;
+      });
+
+      _prayDetail =
+          prefs.setStringList('prayDetail', prayDetail!).then((bool success) {
+        return _prayDetail;
+      });
+    });
+  }
+
+  // 상태 위젯 초기화
+  @override
+  void initState() {
+    super.initState();
+    _prayTitle = _prefs.then(
+        (SharedPreferences prefs) => prefs.getStringList('prayTitle') ?? [""]);
+    _prayDetail = _prefs.then(
+        (SharedPreferences prefs) => prefs.getStringList('prayDetail') ?? [""]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
